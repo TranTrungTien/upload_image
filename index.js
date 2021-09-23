a379de0cd435e903d071c874de27b6fdfbe83d89
TranTrungTien <71311738+TranTrungTien@users.noreply.github.com>
2021-09-23 20:20:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { v4 } = require("uuid");
 const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(cors());
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -18,14 +20,15 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-app.use(cors());
+app.get("/", (req, res) => {
+  res.end("Welcome to upload server");
+})
 
 app.post("/upload", upload.single("avatar"), (req, res) => {
   console.log("running3");
   res.send("saved ...");
 });
-app.get("/download");
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log("Server is running ... ");
 });
